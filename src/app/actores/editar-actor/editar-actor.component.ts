b8ad95e09b5b02bc071878e902468fa7e570acc7
Giomar L <giomarlimocaldas1234@gmail.com>
2022-09-05 00:03:08 -0500

    cambios edit y create

--- src/app/actores/editar-actor/editar-actor.component.ts
+++ src/app/actores/editar-actor/editar-actor.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { actorCreacionDTO } from '../actor';
 
 @Component({
   selector: 'app-editar-actor',
@@ -10,10 +11,19 @@ export class EditarActorComponent implements OnInit {
 
   constructor(private activatedRoute: ActivatedRoute) { }
 
+  modelo: actorCreacionDTO= {
+    nombre: 'Giomar',
+    fechaNacimiento: new Date()
+  }
+
   ngOnInit(): void {
     this.activatedRoute.params.subscribe(params => {
       //alert(params.id);
     });
+  }
+
+  guardarCambios(actor: actorCreacionDTO){
+    console.log(actor);
   }
 
 }
